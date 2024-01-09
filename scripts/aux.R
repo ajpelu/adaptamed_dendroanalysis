7bd84a177fad46bcfbcedbc5b43e2cfe43112ac5
ajpelu <ajpelu@gmail.com>
2024-01-09 17:23:47 +0100

    auxiliary function for colours, etc

---
+++ scripts/aux.R
@@ -0,0 +1,5 @@
+
+# colours
+colours_elev <- c("low" = "red", "low2" ="orange", "medium" = "blue", "high" = "darkgreen")
+
+colours_temp <- c("tmin" = "#a6bddb", "tmed" = "#67a9cf", "tmax" = "#016c59")
