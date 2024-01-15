e94515c6c5120a91ddf8e7a38096268f2f58c10c
ajpelu <ajpelu@gmail.com>
2024-01-15 16:43:00 +0100

    add color code sp

--- scripts/aux.R
+++ scripts/aux.R
@@ -3,3 +3,10 @@
 colours_elev <- c("low" = "red", "low2" ="orange", "medium" = "blue", "high" = "darkgreen")
 
 colours_temp <- c("tmin" = "#a6bddb", "tmed" = "#67a9cf", "tmax" = "#016c59")
+
+colours_sp <- c("pinaster" = "#ffffbf", "nigra" = "#abdda4", "halepensis" = "orange", "sylvestris" = "lightblue")
+
+# pinaster, "#ffffbf", "beige"
+# nigra, "#abdda4", "lightgreen"
+# halepensis, "orange", "orange"
+# sylvestris, "lightblue", "lightblue"
